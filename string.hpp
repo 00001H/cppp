0e121eeab5d7307d7404817e12b60f9d574ca46b
00001H <Programmer00001H@gmail.com>
2025-04-22 18:58:28 +0800

    better u8 support

--- string.hpp
+++ string.hpp
@@ -1,12 +1,17 @@
 #pragma once
+#include<algorithm>
 #include<string>
 namespace cppp{
     using str = std::u8string;
     using sv = std::u8string_view;
-    constexpr inline std::string_view cview(sv x){
-        return {reinterpret_cast<const char*>(x.data()),x.length()};
+    inline std::string_view cview(sv x){
+        return {reinterpret_cast<const char*>(x.data()),x.size()};
     }
-    constexpr inline sv uview(std::string_view x){
-        return {reinterpret_cast<const char8_t*>(x.data()),x.length()};
+    inline sv uview(std::string_view x){
+        return {reinterpret_cast<const char8_t*>(x.data()),x.size()};
+    }
+    constexpr inline sv uview(const std::string&) = delete;
+    inline str tou8(const std::string& s){
+        return {s.begin(),s.end()};
     }
 }
