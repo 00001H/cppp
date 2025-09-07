70c687b6b580d1d6b926eb1f5339bcbe22722624
00001H <Programmer00001H@gmail.com>
2025-09-07 15:04:34 +0800

    code cleanup

--- string.hpp
+++ string.hpp
@@ -4,11 +4,14 @@
 namespace cppp{
     using str = std::u8string;
     using sv = std::u8string_view;
-    inline std::string_view cview(sv x){
-        return {reinterpret_cast<const char*>(x.data()),x.size()};
+    inline std::string_view cview(sv s){
+        return {reinterpret_cast<const char*>(s.data()),s.size()};
     }
-    inline sv uview(std::string_view x){
-        return {reinterpret_cast<const char8_t*>(x.data()),x.size()};
+    inline std::string tocs(str&& s){
+        return std::string(cview(s));
+    }
+    inline sv uview(std::string_view s){
+        return {reinterpret_cast<const char8_t*>(s.data()),s.size()};
     }
     constexpr inline sv uview(const std::string&) = delete;
     inline str tou8(const std::string& s){
